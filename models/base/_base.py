f1c8a2e8e64ced79a3bd28ba82a16ed9c483a81f
Shengtian Sang <77641845+ShengtianSang@users.noreply.github.com>
2022-02-10 14:33:27 -0800

    Add files via upload

---
+++ models/base/_base.py
@@ -0,0 +1,35 @@
+from ._utils import *
+
+class EncoderMixin:
+    """Add encoder functionality such as:
+        - output channels specification of feature tensors (produced by encoder)
+        - patching first convolution for arbitrary input channels
+    """
+
+    @property
+    def out_channels(self):
+        """Return channels dimensions for each tensor of forward output of encoder"""
+        return self._out_channels[: self._depth + 1]
+
+    def set_in_channels(self, in_channels):
+        """Change first convolution channels"""
+        if in_channels == 3:
+            return
+
+        self._in_channels = in_channels
+        if self._out_channels[0] == 3:
+            self._out_channels = tuple([in_channels] + list(self._out_channels)[1:])
+
+        patch_first_conv(model=self, in_channels=in_channels)
+
+    def get_stages(self):
+        """Method should be overridden in encoder"""
+        raise NotImplementedError
+
+    def make_dilated(self, stage_list, dilation_list):
+        stages = self.get_stages()
+        for stage_indx, dilation_rate in zip(stage_list, dilation_list):
+            replace_strides_with_dilation(
+                module=stages[stage_indx],
+                dilation_rate=dilation_rate,
+            )
